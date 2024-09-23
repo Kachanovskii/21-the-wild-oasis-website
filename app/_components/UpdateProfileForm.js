b2214f8697e9182e494a60093844146ccd044faa
Kachanovskii <vadimskyi1@gmail.com>
2024-09-23 14:41:39 +0300

    part 6

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,20 +1,26 @@
 "use client";
 
 import { useState } from "react";
+import { useFormStatus } from "react-dom";
+import { updateGuest } from "../_lib/actions";
+import SubmitButton from "./SubmitButton";
 
-function UpdateProfileForm({children}) {
+function UpdateProfileForm({ guest, children }) {
   const [count, setCount] = useState();
 
-  // CHANGE
-  const countryFlag = "pt.jpg";
-  const nationality = "portugal";
+  const { fullName, email, nationality, nationalID, countryFlag } = guest;
 
   return (
     <div>
-      <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+      <form
+        className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+        action={updateGuest}
+      >
         <div className="space-y-2">
           <label>Full name</label>
           <input
+            defaultValue={fullName}
+            name="fullName"
             disabled
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
           />
@@ -23,6 +29,8 @@ function UpdateProfileForm({children}) {
         <div className="space-y-2">
           <label>Email address</label>
           <input
+            defaultValue={email}
+            name="email"
             disabled
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
           />
@@ -39,21 +47,19 @@ function UpdateProfileForm({children}) {
           </div>
 
           {children}
-
         </div>
 
         <div className="space-y-2">
           <label htmlFor="nationalID">National ID number</label>
           <input
+            defaultValue={nationalID}
             name="nationalID"
             className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
           />
         </div>
 
         <div className="flex justify-end items-center gap-6">
-          <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-            Update profile
-          </button>
+          <SubmitButton pendingLabel='Updating...'>Update reservation</SubmitButton>
         </div>
       </form>
     </div>
--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,11 +1,87 @@
-"use server"
+"use server";
 
-import { signIn, signOut } from "./auth"
+import { revalidatePath } from "next/cache";
+import { revalidate } from "../cabins/page";
+import { auth, signIn, signOut } from "./auth";
+import { supabase } from "./supabase";
+import { redirect } from "next/navigation";
+import { getBookings } from "./data-service";
+
+export async function updateGuest(formData) {
+  const session = await auth();
+
+  if (!session) throw new Error("You must be logged in");
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID))
+    throw new Error("Please provide a valid national ID");
+
+  const updateData = { nationality, countryFlag, nationalID };
+
+  const { data, error } = await supabase
+    .from("guests")
+    .update(updateData)
+    .eq("id", session.user.guestId);
+
+  if (error) throw new Error("Guest could not be updated");
+
+  revalidatePath('/account/profile')
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingsIds = guestBookings.map((booking) => booking.id)
+
+  const { error } = await supabase.from('bookings').delete().eq('id', bookingId);
+
+  if (!guestBookingsIds.includes(bookingId)) throw new Error('You are not allowed to delete his booking')
+
+  if (error) throw new Error('Booking could not be deleted');
+
+  revalidatePath('/account/reservations')
+}
+
+export async function updateBooking(formData) {
+  const bookingId = Number(formData.get('bookingId'));
+  const session = await auth();
+
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingsIds = guestBookings.map((booking) => booking.id)
+
+  if (!guestBookingsIds.includes(bookingId)) throw new Error('You are not allowed to update his booking');
+
+  const updateData = {
+    numGuests: Number(formData.get('numGuests')),
+    observations: formData.get('observations').slice(0, 1000)
+  }
+
+  const { error } = await supabase
+    .from('bookings')
+    .update(updateData)
+    .eq('id', bookingId)
+    .select()
+    .single();
+
+  if (error) throw new Error('Booking could not be updated');
+
+  revalidatePath(`/account/reservations/edit/${bookingId}`);
+  revalidatePath(`/account/reservations`);
+
+  redirect(`/account/reservations`);
+}
 
 export async function signInAction() {
-    await signIn('google', {redirectTo: "/account"})   
+  await signIn("google", { redirectTo: "/account" });
 }
 
 export async function singOutAction() {
-    await signOut({redirectTo: "/"})
+  await signOut({ redirectTo: "/" });
 }

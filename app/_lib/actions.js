1159f23639d0d786ab7310af151c7a4acf6b2d18
Kachanovskii <vadimskyi1@gmail.com>
2024-09-24 16:28:36 +0300

    part 7

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,11 +1,10 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
-import { revalidate } from "../cabins/page";
 import { auth, signIn, signOut } from "./auth";
-import { supabase } from "./supabase";
-import { redirect } from "next/navigation";
 import { getBookings } from "./data-service";
+import { supabase } from "./supabase";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -30,7 +29,32 @@ export async function updateGuest(formData) {
   revalidatePath('/account/profile')
 }
 
-export async function deleteReservation(bookingId) {
+export async function createBooking(bookingData, formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const newBooking = {
+    ...bookingData,
+    guestId: session.user.guestId,
+    numGuests: Number(formData.get("numGuests")),
+    observations: formData.get("observations").slice(0, 1000),
+    extrasPrice: 0,
+    totalPrice: bookingData.cabinPrice,
+    isPaid: false,
+    hasBreakfast: false,
+    status: "unconfirmed",
+  };
+
+  const { error } = await supabase.from("bookings").insert([newBooking]);
+
+  if (error) throw new Error("Booking could not be created");
+
+  revalidatePath(`/cabins/${bookingData.cabinId}`);
+
+  redirect("/cabins/thankyou");
+}
+
+export async function deleteBooking(bookingId) {
   const session = await auth();
 
   if (!session) throw new Error("You must be logged in");
